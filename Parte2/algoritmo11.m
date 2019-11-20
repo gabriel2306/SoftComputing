82ff0d6445c0a06fd17493184bb97b0694063f65
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-11-20 12:13:37 +0100

    varias funciones

--- Parte2/algoritmo11.m
+++ Parte2/algoritmo11.m
@@ -1,15 +1,15 @@
-function [soluciones,mejor] = algoritmo11(vector,gmax)
-    minimoActual = funcion1(vector);
+function [soluciones,mejor] = algoritmo11(vector,gmax,nFuncion,sigma)
     padre = vector;
+    minimoActual = evaluarFuncion(padre,nFuncion);
+    
     mejor = minimoActual;
     soluciones = [];
-    hijo = [];
-    varianza = 0.5;
+    varianza = sigma;
     
     for i=1:gmax
         soluciones = [soluciones; minimoActual];
         hijo = generarHijo(padre,varianza);
-        minimoHijo = funcion1(hijo);
+        minimoHijo = evaluarFuncion(hijo,nFuncion);
         
         if(minimoHijo<minimoActual)
             minimoActual = minimoHijo;
@@ -22,4 +22,18 @@ end
 function [hijo] = generarHijo(padre,varianza)
     ruido = 0 + varianza*randn(1,30);
     hijo = padre + ruido;
+end
+
+function [eval] = evaluarFuncion(vector, nFuncion)
+    aux = 0;
+    switch nFuncion
+        case 1
+            aux = funcion1(vector);
+        case 2
+            aux = funcion2(vector);
+        case 3
+            aux = funcion3(vector);
+    end
+    
+    eval = aux;
 end
--- Parte2/prueba_11.m
+++ Parte2/prueba_11.m
@@ -1,6 +1,7 @@
 vector = -100 + 200*rand(1,30);
 maxGeneraciones = 10000;
-[soluciones,mejor] = algoritmo11(vector,maxGeneraciones);
+sigma = 0.5;
+[soluciones,mejor] = algoritmo11(vector,maxGeneraciones,1,sigma);
 
 plot(soluciones);
 mejorFitness = mejor;
