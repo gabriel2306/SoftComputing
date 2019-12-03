b838b3a8ad93445313344b829dec7969befef48f
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-12-03 17:11:51 +0100

    pruebas

---
+++ Parte2/ejecucion.m
@@ -0,0 +1,108 @@
+nFuncion = 1;
+
+if (nFuncion==1)
+    limiteInferior = -100;
+    limiteSuperior = 100;
+end
+
+if (nFuncion==2)
+    limiteInferior = -500;
+    limiteSuperior = 500;
+end
+    
+if (nFuncion==3)
+    limiteInferior = -30;
+    limiteSuperior = 30;
+end
+
+vector = limiteInferior + (limiteSuperior-limiteInferior)*rand(1,30);
+sigma = 0.5;
+
+maxGeneraciones11 = 10000;
+
+tic
+[allFitness,mejorFitness,solucion] = algoritmo11(vector,maxGeneraciones11,nFuncion,sigma);
+bestFitness11 = mejorFitness;
+allFitness11 = allFitness;
+solucion11 = solucion;
+fitnessIteraciones11 = [mejorFitness];
+for i=1:30
+    [allFitness,mejorFitness,solucion] = algoritmo11(vector,maxGeneraciones11,nFuncion,sigma);
+    fitnessIteraciones11 = [fitnessIteraciones11 mejorFitness];
+    if (mejorFitness<bestFitness11)
+        bestFitness11 = mejorFitness;
+        allFitness11 = allFitness;
+        solucion11 = solucion;
+    end 
+end
+toc
+
+nVectores = 10;
+vectores = cell(1,nVectores);
+nHijos = 10;
+for i=1:nVectores
+    vector = limiteInferior + (limiteSuperior-limiteInferior)*rand(1,30);
+    vectores{i} = vector;
+end
+
+maxGeneracionesML = 10000;
+
+tic
+[allFitness,mejorFitness,solucion] = algoritmoML(vectores,maxGeneracionesML,nFuncion,sigma,nHijos);
+bestFitnessML = mejorFitness;
+allFitnessML = allFitness;
+solucionML = solucion;
+fitnessIteracionesML = [mejorFitness];
+for i=1:30
+    [allFitness,mejorFitness,solucion] = algoritmoML(vectores,maxGeneracionesML,nFuncion,sigma,nHijos);
+    fitnessIteracionesML = [fitnessIteracionesML mejorFitness];
+    if (mejorFitness<bestFitnessML)
+        bestFitnessML = mejorFitness;
+        allFitnessML = allFitness;
+        solucionML = solucion;
+    end 
+end
+toc
+
+maxGeneracionesHS = 10000;
+
+tic
+[allFitness,mejorFitness,solucion] = algoritmoHS(vectores,maxGeneracionesHS,nFuncion,sigma);
+bestFitnessHS = mejorFitness;
+allFitnessHS = allFitness;
+solucionHS = solucion;
+fitnessIteracionesHS = [mejorFitness];
+for i=1:30
+    [allFitness,mejorFitness,solucion] = algoritmoML(vectores,maxGeneracionesHS,nFuncion,sigma,nHijos);
+    fitnessIteracionesHS = [fitnessIteracionesHS mejorFitness];
+    if (mejorFitness<bestFitnessHS)
+        bestFitnessHS = mejorFitness;
+        allFitnessHS = allFitness;
+        solucionHS = solucion;
+    end 
+end
+toc
+
+subplot(3,1,1)
+plot(allFitness11);
+title('1+1');
+disp('Mejor');
+disp(bestFitness11);
+disp('Media');
+disp(mean(fitnessIteraciones11));
+
+subplot(3,1,2);
+plot(allFitnessML);
+title('M+L');
+disp('Mejor');
+disp(bestFitnessML);
+disp('Media');
+disp(mean(fitnessIteracionesML));
+
+subplot(3,1,3);
+plot(allFitnessHS);
+title('HS');
+disp('Mejor');
+disp(bestFitnessHS);
+disp('Media');
+disp(mean(fitnessIteracionesHS));
